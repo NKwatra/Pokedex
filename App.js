e2d778172259edbd5ec241ad6e0b9941c60abd61
NKwatra <kwatranishkarsh@gmail.com>
2020-12-17 10:24:03 +0530

    Load more pokemon on reaching list end

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, FlatList, Text, Switch} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Text,
+  Switch,
+  ActivityIndicator,
+} from 'react-native';
 import Loading from './src/components/Loading';
 import PokeCard from './src/components/PokeCard';
 import {getPokemonData} from './src/utils/network';
@@ -9,6 +16,7 @@ export default function App() {
   const [offset, setOffset] = useState(0);
   const [pokemonList, setPokemonList] = useState([]);
   const [numColumns, setNumColums] = useState(1);
+  const [loadingMorePokemon, setLoadingMorePokemon] = useState(false);
 
   useEffect(() => {
     getPokemonData(offset).then((data) => {
@@ -19,9 +27,13 @@ export default function App() {
         Please check your internet connection if the  problem persists.`,
         );
       } else {
-        setPokemonList(data);
+        setPokemonList((currentPokemonList) => [
+          ...currentPokemonList,
+          ...data,
+        ]);
       }
       setLoading(false);
+      setLoadingMorePokemon(false);
     });
   }, [offset]);
 
@@ -35,20 +47,32 @@ export default function App() {
         <Text>Comfortable</Text>
         <Switch
           value={numColumns === 2}
-          onValueChange={() =>
-            setNumColums((columns) => (columns === 1 ? 2 : 1))
-          }
+          onValueChange={() => {
+            setNumColums((columns) => (columns === 1 ? 2 : 1));
+          }}
           style={styles.switch}
         />
         <Text>Compact</Text>
       </View>
       <FlatList
         data={pokemonList}
-        keyExtractor={(item) => item.url}
+        keyExtractor={(item) => item.name}
         renderItem={renderItem}
         numColumns={numColumns}
         key={numColumns}
+        onEndReachedThreshold={0.1}
+        onEndReached={() => {
+          setLoadingMorePokemon(true);
+          setOffset((currentOffset) => currentOffset + 20);
+        }}
       />
+      {loadingMorePokemon ? (
+        <ActivityIndicator
+          size="small"
+          color="#0000ff"
+          style={styles.moreLoading}
+        />
+      ) : null}
     </View>
   );
 }
@@ -62,7 +86,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
   switch: {
     marginHorizontal: 16,
+  },
+
+  moreLoading: {
+    marginBottom: 8,
   },
 });
